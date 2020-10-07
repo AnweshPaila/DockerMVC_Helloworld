9d5134a0dc27d9947f19c847199cf5a52e069424
AnweshPaila <anwesh1729@gmail.com>
2020-10-07 19:27:21 +0530

    Add project files.

---
+++ DockerMVC_Helloworld/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using DockerMVC_Helloworld.Models.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DockerMVC_Helloworld.Controllers
+{
+    public class HomeController : Controller
+    {
+        DockerDBEntities db = new DockerDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Names = db.DockerTables.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name)
+        {
+            var obj = new DockerTable() { Name = name };
+            db.DockerTables.Add(obj);
+            db.SaveChanges();
+            ViewBag.Names = db.DockerTables.ToList();
+            return View();
+        }
+
+    }
+}
---
+++ DockerMVC_Helloworld/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DockerMVC_Helloworld")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DockerMVC_Helloworld")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1806718-c1ea-4603-bb9f-7c2d0769f16e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
